2e915b7751deede210ca6bea0a3c11986009fd58
0VERT <tan.tanapat2002@gmail.com>
2025-06-25 12:24:51 +0700

    add app.py

---
+++ HELLOWORLD/app.py
@@ -0,0 +1,56 @@
+# course = ("Python Progamming")
+# print(len(course))
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[0])
+
+# EscapeSequences
+# course = 'Python \n"Progamming'
+# print(course)
+
+# Format String
+# first = "Tanapat"
+# last = "Nukool"
+# # full = first + " " + last
+# full = f"{len(first)} {last}"
+# print(full)
+
+# String Method
+# course = "     Python Progamming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("Pro"))
+# print(course.replace("P", "j"))
+# print("Pro" in course)
+# print("Swift" not in course)
+
+# Number
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# Working with Number
+# import math
+# print(round(2.9))
+# print(abs(-2.9))
+
+# # Ceiling of x, the smallest integer greater than or equal to x
+# print("Ceil", math.ceil(2.2))
+# # Absolute value of x
+# print("Fabs", math.fabs(-2.2))
+# # The largest integer less than or equal to x.
+# print("Floor", math.floor(2.2))
+
+# Type Conversion
+x = input("x:")
+y = int(x) + 1
+print(f"x:{x}, y:{y}")
+# print(type(x))
+# y = x + 1
